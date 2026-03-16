813654fd3d3f1d459eb3c89ae043c9fbd3dc02f8
Auto Commit <auto@example.com>
2026-03-16 22:00:21 +0530

    Added DevOps setup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+  try {
+    if (!process.env.MONGODB_URI) {
+      console.log("MongoDB URI not provided. Running without DB.");
+      return;
     }
+
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection failed, continuing without DB");
+  }
 };
 
 export default connectDB;
